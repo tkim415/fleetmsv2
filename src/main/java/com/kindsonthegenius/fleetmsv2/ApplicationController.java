252a38f583cf8bc2f8d03f9336155057a5d35f2f
tkim415 <thomasdkim@gmail.com>
2024-06-15 11:41:06 -0700

    updated index.html to use the layout.html

--- src/main/java/com/kindsonthegenius/fleetmsv2/ApplicationController.java
+++ src/main/java/com/kindsonthegenius/fleetmsv2/ApplicationController.java
@@ -11,8 +11,43 @@ public class ApplicationController {
 		return "index";
 	}
 
-	@GetMapping("/_layout")
+	@GetMapping("/index2")
 	public String widget() {
-		return "_layout";
+		return "index2";
+	}
+
+	@GetMapping("/layout")
+	public String layout() {
+		return "layout";
+	}
+
+	@GetMapping("/hr")
+	public String hr() {
+		return "/hr/index";
+	}
+
+	@GetMapping("/fleet")
+	public String fleet() {
+		return "/fleet/index";
+	}
+
+	@GetMapping("/helpdesk")
+	public String helpdesk() {
+		return "/helpdesk/index";
+	}
+
+	@GetMapping("/accounts")
+	public String accounts() {
+		return "/accounts/index";
+	}
+
+	@GetMapping("/payroll")
+	public String payroll() {
+		return "/payroll/index";
+	}
+
+	@GetMapping("/parameters")
+	public String parameters() {
+		return "/parameters/index";
 	}
 }
